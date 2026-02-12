bb2b26700c74a73bb1eb574d169a3898b49daab9
vittolym <lexymdz@gmail.com>
2026-02-12 20:55:35 -0300

    Feat: add register

--- user-service/src/modules/auth/auth.controller.ts
+++ user-service/src/modules/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from '../auth/dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,9 @@ export class AuthController {
   @Post('login')
   login(@Body() data: LoginUserDto) {
     return this.authService.login(data);
+  }
+  @Post('register')
+  regisrter(@Body() data: RegisterDto) {
+    console.log(data);
   }
 }
