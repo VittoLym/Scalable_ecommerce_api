a99057385db2b5aa3fc049c6a51106cc3983758d
vittolym <lexymdz@gmail.com>
2026-03-21 23:01:07 -0300

    add product gateway

--- gateway/src/modules/products/products.controller.ts
+++ gateway/src/modules/products/products.controller.ts
@@ -1,23 +1,149 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  Query,
+  Req,
+  UseGuards,
+  HttpException,
+} from '@nestjs/common';
+import type { Request } from 'express';
 import { proxyRequest } from '../../common/interceptor/proxy.interceptor';
+import axios from 'axios';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from '@prisma/client';
+
+const PRODUCT_SERVICE =
+  process.env.PRODUCT_SERVICE_URL || 'http://localhost:3002';
 
 @Controller('products')
-export class ProductsController {
+export class ProductController {
   @Get('health')
-  @HttpCode(HttpStatus.OK)
-  healthStatus(): object {
-    return {
-      message: 'hola',
-      status: 'Products is Done',
-    };
+  health(@Req() req: Request) {
+    try {
+      const response = proxyRequest(
+        'GET',
+        `${PRODUCT_SERVICE}/products/health`,
+        null,
+        {
+          headers: {
+            authorization: req.headers.authorization,
+          },
+        },
+      );
+
+      return response;
+    } catch (error) {
+      throw this.handleError(error);
+    }
+  }
+  @Post()
+  create(@Body() body, @Req() req: Request) {
+    try {
+      const response = proxyRequest(
+        'POST',
+        `${PRODUCT_SERVICE}/products`,
+        body,
+        {
+          headers: {
+            authorization: req.headers.authorization,
+          },
+        },
+      );
+
+      return response;
+    } catch (error) {
+      throw this.handleError(error);
+    }
   }
   @Get()
-  getAll() {
-    return proxyRequest(
-      'GET',
-      `${process.env.PRODUCT_SERVICE_URL}/products`,
-      null,
-      {},
-    );
+  findAll(@Query() query, @Req() req: Request) {
+    try {
+      const response = proxyRequest(
+        'GET',
+        `${PRODUCT_SERVICE}/products`,
+        null,
+        {
+          params: query,
+          headers: {
+            authorization: req.headers.authorization,
+          },
+        },
+      );
+
+      return response;
+    } catch (error) {
+      throw this.handleError(error);
+    }
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string, @Req() req: Request) {
+    try {
+      const response = proxyRequest(
+        'GET',
+        `${PRODUCT_SERVICE}/products/${id}`,
+        null,
+        {
+          headers: {
+            authorization: req.headers.authorization,
+          },
+        },
+      );
+
+      return response.data;
+    } catch (error) {
+      throw this.handleError(error);
+    }
+  }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body, @Req() req: Request) {
+    try {
+      const response = proxyRequest(
+        'PUT',
+        `${PRODUCT_SERVICE}/products/${id}`,
+        body,
+        {
+          headers: {
+            authorization: req.headers.authorization,
+          },
+        },
+      );
+
+      return response;
+    } catch (error) {
+      throw this.handleError(error);
+    }
+  }
+  @Delete(':id')
+  remove(@Param('id') id: string, @Req() req: Request) {
+    try {
+      const response = proxyRequest(
+        'DELETE',
+        `${PRODUCT_SERVICE}/products/${id}`,
+        null,
+        {
+          headers: {
+            authorization: req.headers.authorization,
+          },
+        },
+      );
+
+      return response;
+    } catch (error) {
+      throw this.handleError(error);
+    }
+  }
+  private handleError(error: any) {
+    if (error.response) {
+      return new HttpException(error.response.data,error.response.status);
+    }
+
+    return new HttpException('Error en API Gateway', 500);
   }
 }
